b46046fdfa1067cf4e1b854e45afbe599a491e9b
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-04-25 14:03:15 +0300

    Урок по геолокации и контактам

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,7 @@
 from .help import dp
 from .start import dp
+from .get_contact import dp
+from .get_location import dp
 from .echo import dp
 
 __all__ = ["dp"]
---
+++ handlers/users/get_location.py
@@ -0,0 +1,33 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+from keyboards.default import location_button
+from utils.misc.calc_distance import choose_shortest
+
+from loader import dp, bot
+
+
+@dp.message_handler(Command("show_on_map"))
+async def share_number(message: types.Message):
+    await message.answer(f"Здравствуйте, {message.from_user.full_name}.\n"
+                         f"Для того, чтобы показать ближайшие магазины отправьте нам свою локацию "
+                         f"нажав на кнопку ниже!", reply_markup=location_button.keyboard)
+
+
+@dp.message_handler(content_types=types.ContentType.LOCATION)
+async def get_contact(message: types.Message):
+    location = message.location
+    latitude = location.latitude
+    longitude = location.longitude
+    closest_shops = choose_shortest(location)
+
+    text = "\n\n".join([f"Название: {shop_name}. <a href='{url}'>Google</a>\n Расстояние до него: {distance:.1f} км."
+                        for shop_name, distance, url, shop_location in closest_shops])
+
+    await message.answer(f"Спасибо. \n"
+                         f"Latitude = {latitude}\n"
+                         f"Longitude = {longitude}\n\n"
+                         f"{text}", disable_web_page_preview=True)
+
+    for shop_name, distance, url, shop_location in closest_shops:
+        await message.answer_location(latitude=shop_location["lat"],
+                                      longitude=shop_location["lon"])
