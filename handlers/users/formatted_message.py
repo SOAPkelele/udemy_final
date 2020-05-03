a8602a545ef3a98736c5dc655d9ee7a158bdd22a
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-05-03 15:35:42 +0300

    Обновил текст для форматирования

--- handlers/users/formatted_message.py
+++ handlers/users/formatted_message.py
@@ -15,18 +15,10 @@ html_text = "\n".join(
                                                                              "https://core.telegram.org/bots/api#formatting-options"),
         hunderline("Внимательно прочти и используй с умом!"),
         "",
-        "Пример использования - ниже:",
-        hcode("""<b>bold</b>, <strong>bold</strong>
-<i>italic</i>, <em>italic</em>
-<u>underline</u>, <ins>underline</ins>
-<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
-<b>bold <i>italic bold <s>italic bold strikethrough</s> <u>underline italic bold</u></i> bold</b>
-<a href="http://www.example.com/">inline URL</a>
-<a href="tg://user?id=123456789">inline mention of a user</a>
-<code>inline fixed-width code</code>
-<pre>pre-formatted fixed-width code block</pre>
-<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>""")
-    ])
+        hcode("Пример использования - ниже:"),
+        "\n\n"]
+)
+html_text += hcode(html_text)
 
 
 @dp.message_handler(Command("parse_mode_html"))
