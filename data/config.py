af3df355740605bd0f0a494753af58a0c1e54d4f
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-05-05 23:45:13 +0300

    Работа с платежками встроенными

--- data/config.py
+++ data/config.py
@@ -19,3 +19,5 @@ redis = {
     'address': (ip, 6379),
     'encoding': 'utf8'
 }
+
+PROVIDER_TOKEN = "410694247:TEST:3e5c1773-2ebe-4c1d-b744-543c3798ad7e"
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,6 @@
-from .help import dp
-from .start import dp
-from .echo import dp
+# from .help import dp
+# from .start import dp
+# from .echo import dp
+from .create_order import dp
 
 __all__ = ["dp"]
