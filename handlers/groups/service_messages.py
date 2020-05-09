a74c474da706440c7d0117e55e0802aa8b402fce
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-05-09 09:28:59 +0300

    Bug fix

--- handlers/groups/service_messages.py
+++ handlers/groups/service_messages.py
@@ -6,11 +6,10 @@ from filters import IsGroup
 
 @dp.message_handler(IsGroup(), content_types=types.ContentType.NEW_CHAT_MEMBERS)
 async def new_member(message: types.Message):
-    await message.reply(f"Привет, {message.from_user.full_name}.")
+    await message.reply(f"Привет, {message.new_chat_members[0].full_name}.")
 
 
 @dp.message_handler(IsGroup(), content_types=types.ContentType.LEFT_CHAT_MEMBER)
 async def banned_member(message: types.Message):
     await message.answer(f"{message.left_chat_member.full_name} был удален из чата "
                          f"пользователем {message.from_user.full_name}.")
-
