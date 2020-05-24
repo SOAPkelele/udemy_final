00b3dfb88478411ed7190cfbcf25d05a5a7c0b7f
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-05-24 12:32:11 +0300

    Работа с Middleware

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,8 +2,13 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from utils.db_api.user import User
+from utils.misc import rate_limit
 
 
+# Можно запускать раз в 10 сек
+@rate_limit(limit=10)
 @dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!')
+async def bot_start(message: types.Message, user: User):
+    await message.answer(f'Привет, {message.from_user.full_name}!\n'
+                         f'Юзер полученный через миддлварь: {user.__dict__}')
