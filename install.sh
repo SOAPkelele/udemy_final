d0b87fc645283c373ff8339288db143307b59b9f
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-05-25 23:30:52 +0300

    Деплой на сервер и запуск бота через supervisorctl

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+# Устанавливаем supervisorctl
+apt-get update &&\
+sudo apt-get install python3 &&\
+apt-get install supervisor -y &&\
 # Создаем директорию telegrambot в /home
-mkdir -p /home/telegrambot
+mkdir -p /home/telegrambot &&\
 # Перемещаем все что в текущей директории в эту папку
-mv -R . /home/telegrambot
+mv  ./* /home/telegrambot &&\
+mv ./.env.dist /home/telegrambot/.env &&\
 # Заходим в эту папку
-cd /home/telegrambot
+cd /home/telegrambot &&\
 # Перемещаем файл конфига в папку супервайзера
-mv bot.conf /etc/supervisor/conf.d/bot.conf
+mv bot.conf /etc/supervisor/conf.d/bot.conf &&\
+apt install python3-pip &&\
+python3 -m pip install -r requirements.txt -y &&\
+
 # Теперь пусть супервайзер подгрузит новый кофиг
-supervisorctl reread
+supervisorctl reread &&\
 supervisorctl update
+# На этом этапе бот не заработает, потому что нужно прописать все данные в .env и после этого перезапустить в супервайзере
