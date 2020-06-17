0d7a64b7fb37502d8eea7adbf53c01a90bab83dc
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-06-18 00:11:41 +0300

    фикс

--- handlers/groups/edit_chat.py
+++ handlers/groups/edit_chat.py
@@ -1,13 +1,14 @@
 import io
 
 from aiogram import types
+from aiogram.dispatcher.filters import Command
 
-from loader import dp, bot
 from filters import IsGroup
-from aiogram.dispatcher.filters import AdminFilter, Command
+from filters.admins import AdminFilter
+from loader import dp, bot
 
 
-@dp.message_handler(IsGroup(), AdminFilter(), Command("set_photo", prefixes="!/"))
+@dp.message_handler(IsGroup(), Command("set_photo", prefixes="!/"), AdminFilter())
 async def set_new_photo(message: types.Message):
     source_message = message.reply_to_message
     photo = source_message.photo[-1]
@@ -19,7 +20,7 @@ async def set_new_photo(message: types.Message):
     await message.chat.set_photo(photo=input_file)
 
 
-@dp.message_handler(IsGroup(), AdminFilter(), Command("set_title", prefixes="!/"))
+@dp.message_handler(IsGroup(), Command("set_title", prefixes="!/"), AdminFilter())
 async def set_new_title(message: types.Message):
     source_message = message.reply_to_message
     title = source_message.text
@@ -30,7 +31,7 @@ async def set_new_title(message: types.Message):
     await message.chat.set_title(title=title)
 
 
-@dp.message_handler(IsGroup(), AdminFilter(), Command("set_description", prefixes="!/"))
+@dp.message_handler(IsGroup(), Command("set_description", prefixes="!/"), AdminFilter())
 async def set_new_description(message: types.Message):
     source_message = message.reply_to_message
     description = source_message.text
--- handlers/groups/moderate_chat.py
+++ handlers/groups/moderate_chat.py
@@ -4,18 +4,20 @@ import re
 
 import aiogram
 from aiogram import types
+from aiogram.dispatcher.filters import Command
+from aiogram.utils.exceptions import BadRequest
 
-from loader import bot, dp
-from aiogram.dispatcher.filters import Command, AdminFilter
-from filters import IsGroup
+from filters import IsGroup, AdminFilter
+from loader import dp, bot
 
 
 # Хендлер с фильтром в группе, где можно использовать команду !ro ИЛИ /ro
-@dp.message_handler(IsGroup(), AdminFilter(), Command(commands=["ro"], prefixes="!/"))
+@dp.message_handler(IsGroup(), Command("ro", prefixes="!/"), AdminFilter())
 async def read_only_mode(message: types.Message):
-    member = message.reply_to_message.from_user.id
-    chat = message.chat.id
-    command_parse = re.compile(r"(!ro|/ro) ?(\d+)? ?([a-zA-Z ]+)?")
+    member = message.reply_to_message.from_user
+    member_id = member.id
+    chat_id = message.chat.id
+    command_parse = re.compile(r"(!ro|/ro) ?(\d+)? ?([\w+\D]+)?")
     parsed = command_parse.match(message.text)
     time = parsed.group(2)
     comment = parsed.group(3)
@@ -51,7 +53,7 @@ async def read_only_mode(message: types.Message):
 
     # Вариант 2 - сокращенный
     try:
-        await message.chat.restrict(user_id=member, can_send_messages=False, until_date=until_date)
+        await message.chat.restrict(user_id=member_id, can_send_messages=False, until_date=until_date)
         await message.reply_to_message.delete()
     except aiogram.utils.exceptions.BadRequest as err:
         await message.answer(f"Ошибка! {err.args}")
@@ -67,71 +69,75 @@ async def read_only_mode(message: types.Message):
 
     # Удаляем сообщения
     # Вариант 1 - по API
-    # await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    # await bot.delete_message(chat_id=chat_id, message_id=message.message_id)
 
     # Вариант 2 - сокращенный
     await message.delete()
     await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), AdminFilter(), Command(commands=["unro"], prefixes="!/"))
+@dp.message_handler(IsGroup(), Command("unro", prefixes="!/"), AdminFilter())
 async def undo_read_only_mode(message: types.Message):
-    member = message.reply_to_message.from_user.id
-    chat = message.chat.id
-    await bot.restrict_chat_member(chat_id=chat, user_id=member,
-                                   can_send_messages=True,
-                                   can_add_web_page_previews=True,
-                                   can_send_media_messages=True,
-                                   can_send_other_messages=True)
+    member = message.reply_to_message.from_user
+    member_id = member.id
+    chat_id = message.chat.id
 
-    await message.answer(f"Пользователь {message.reply_to_message.from_user.full_name} был разбанен")
-
+    user_allowed = types.ChatPermissions(
+        can_send_messages=True,
+        can_send_media_messages=True,
+        can_send_polls=True,
+        can_send_other_messages=True,
+        can_add_web_page_previews=True,
+        can_invite_users=True,
+        can_change_info=False,
+        can_pin_messages=False,
+    )
     service_message = await message.reply("Сообщение самоуничтожится через 5 секунд.")
     # Пауза 5 сек
     await asyncio.sleep(5)
 
     # Удаляем сообщения
     # Вариант 1 - по API
-    # await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    # await bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+
+    await message.chat.restrict(user_id=member_id, permissions=user_allowed, until_date=0)
+    await message.reply(f"Пользователь {member.full_name} был разбанен")
 
     # Вариант 2 - сокращенный
     await message.delete()
     await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), AdminFilter(), Command(commands=["ban"], prefixes="!/"))
+@dp.message_handler(IsGroup(), Command("ban", prefixes="!/"), AdminFilter())
 async def ban_user(message: types.Message):
-    member = message.reply_to_message.from_user.id
-    # Вариант 1- по API
-    # chat = message.chat.id
-    # await bot.kick_chat_member(chat_id=chat, user_id=member)
+    member = message.reply_to_message.from_user
+    member_id = member.id
+    chat_id = message.chat.id
 
-    # Ваирант 2 - сокращенный
-    await message.chat.kick(user_id=member)
+    # Вариант 1 - по API
+    # await bot.kick_chat_member(chat_id=chat_id, user_id=member)
 
+    # Вариант 2 - сокращенный
+    await message.chat.kick(user_id=member_id)
+
+    # Пишем в чат
+    await message.answer(f"Пользователь {message.reply_to_message.from_user.full_name} был забанен")
     service_message = await message.reply("Сообщение самоуничтожится через 5 секунд.")
+
     # Пауза 5 сек
     await asyncio.sleep(5)
 
     # Удаляем сообщения
-    # Вариант 1 - по API
-    # await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-
-    # Вариант 2 - сокращенный
     await message.delete()
     await service_message.delete()
 
 
-@dp.message_handler(IsGroup(), AdminFilter(), Command(commands=["unban"], prefixes="!/"))
-async def ban_user(message: types.Message):
-    member = message.reply_to_message.from_user.id
-    chat = message.chat.id
-    # Вариант 1 - по API
-    # await bot.unban_chat_member(chat_id=chat, user_id=member)
-
-    # Вариант 2 - сокращенный
-    await message.chat.unban(user_id=member)
-
+@dp.message_handler(IsGroup(), Command("unban", prefixes="!/"), AdminFilter())
+async def unban_user(message: types.Message):
+    member = message.reply_to_message.from_user
+    member_id = member.id
+    chat_id = message.chat.id
+    await message.chat.unban(user_id=member_id)
     # Пишем в чат
     await message.answer(f"Пользователь {message.reply_to_message.from_user.full_name} был разбанен")
     service_message = await message.reply("Сообщение самоуничтожится через 5 секунд.")
