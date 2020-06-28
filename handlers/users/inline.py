453f98cb425e5592f26ee9d8b61e55e97caa4d5d
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-06-28 11:15:18 +0300

    фикс

--- handlers/users/inline.py
+++ handlers/users/inline.py
@@ -7,14 +7,19 @@ from loader import dp, bot
 
 @dp.inline_handler(text="")
 async def empty_query(query: types.InlineQuery):
-    user = query.from_user.id
-    if user not in allowed_users:
-        await query.answer(
-            results=[],
-            switch_pm_text="Бот недоступен. Подключить бота",
-            switch_pm_parameter="connect_user",
-            cache_time=5)
-        return
+    await query.answer(
+        results=[
+            types.InlineQueryResultArticle(
+                id="unknown",
+                title="Введите какой-то запрос",
+                input_message_content=types.InputTextMessageContent(
+                    message_text="Не обязательно жать при этом на кнопку",
+                    parse_mode="HTML"
+                ),
+            ),
+        ],
+
+        cache_time=5)
 
 
 @dp.inline_handler()
@@ -40,12 +45,12 @@ async def empty_query(query: types.InlineQuery):
                 thumb_url="https://apps-cdn.athom.com/app/org.telegram.api.bot/1/1c9f8d07-be07-442d-933d-16fd212a68f1/assets/images/large.png",
                 description="Описание, в инлнайн режиме"
             ),
-            types.InlineQueryResultCachedPhoto(
-                id="2",
-                photo_file_id="AgACAgIAAxkBAAICcV6jF5kAARvDMn99PQuVe9fBg-TKcAACQ64xG0WQGEm4F3v9dsbAAg7Hwg8ABAEAAwIAA3kAA9c_BgABGQQ",
-                description="Описание, которое нигде не отображается!",
-                caption="Тут будет подпись, которая отправится с картинкой, если на нее нажать",
-            ),
+            # types.InlineQueryResultCachedPhoto(
+            #     id="2",
+            #     photo_file_id="AgACAgIAAxkBAAICcV6jF5kAARvDMn99PQuVe9fBg-TKcAACQ64xG0WQGEm4F3v9dsbAAg7Hwg8ABAEAAwIAA3kAA9c_BgABGQQ",
+            #     description="Описание, которое нигде не отображается!",
+            #     caption="Тут будет подпись, которая отправится с картинкой, если на нее нажать",
+            # ),
             types.InlineQueryResultVideo(
                 id="4",
                 video_url="https://pixabay.com/en/videos/download/video-10737_medium.mp4",
