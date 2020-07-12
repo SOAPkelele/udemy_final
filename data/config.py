a6cf7879be001f735460463b580c90217d9fb407
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-07-12 14:31:59 +0300

    фикс

--- data/config.py
+++ data/config.py
@@ -3,13 +3,17 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# https://plugins.jetbrains.com/plugin/12798-tabnine
+# https://qiwi.com/api
+# https://qiwi.com/p2p-admin/transfers/api
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+
 QIWI_TOKEN = os.getenv("qiwi")
 WALLET_QIWI = os.getenv("wallet")
 QIWI_PUBKEY = os.getenv("qiwi_p_pub")
+
 admins = [
-    362089194
+    517563938
 ]
 
 ip = os.getenv("ip")
