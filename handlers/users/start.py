526184d2b4ddb4bade44ea383263bc3d4a6eb80c
Kostiantyn Kriuchkov <latand666@gmail.com>
2020-07-22 21:11:24 +0300

    фикс

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
@@ -11,7 +9,7 @@ from utils.db_api import quick_commands as db
 async def bot_start(message: types.Message):
     name = message.from_user.full_name
     await db.add_user(id=message.from_user.id,
-                                  name=name)
+                      name=name)
 
     count = await db.count_users()
     await message.answer(
