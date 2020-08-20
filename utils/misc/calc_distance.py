e62c052b620aa2fb66584d6ff8f3bddc74c662ff
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-08-20 19:24:24 +0300

    Update calc_distance.py

--- utils/misc/calc_distance.py
+++ utils/misc/calc_distance.py
@@ -7,24 +7,16 @@ from data.locations import Shops
 
 R = 6378.1
 
-
 def calc_distance(lat1, lon1, lat2, lon2):
-    lat1 = math.radians(lat1)
-    lon1 = math.radians(lon1)
-
-    lat2 = math.radians(lat2)
-    lon2 = math.radians(lon2)
-
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-
-    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
-
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    distance = R * c
-
-    print(distance)
-    return distance
+    a = sin(dlat / 2)  2 + cos(lat1) * cos(lat2) * sin(dlon / 2)  2
+    c = 2 * asin(sqrt(a))
+    # Radius of earth in kilometers is 6371
+    km = 6371 * c
+    return ceil(km * 1000)
 
 
 def choose_shortest(location: types.Location):
