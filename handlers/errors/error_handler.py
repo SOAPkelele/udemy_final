423f46215a05aab59bce1bf09edd4d6e899ef579
Kostiantyn Kriuchkov <36363097+Latand@users.noreply.github.com>
2020-08-30 23:13:45 +0300

    Update error_handler.py

--- handlers/errors/error_handler.py
+++ handlers/errors/error_handler.py
@@ -54,7 +54,7 @@ async def errors_handler(update, exception):
         logging.exception(f'RetryAfter: {exception} \nUpdate: {update}')
         return True
     if isinstance(exception, BadRequest):
-        logging.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
+        logging.exception(f'BadRequest: {exception} \nUpdate: {update}')
         return True
     if isinstance(exception, TelegramAPIError):
         logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
